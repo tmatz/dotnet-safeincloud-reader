341b4b4b9616ac0e06647add0f5598ee7793924f
MATSUDA Takashi <t.matsuda.1000@gmail.com>
2019-09-28 17:20:05 +0900

    work!

---
+++ SafeInCloudReader/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+namespace SafeInCloudReader
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("wong number of argument. filePath and password are required.");
+                return 1;
+            }
+
+            try
+            {
+                string path = args[0];
+                string password = args[1];
+
+                if (!File.Exists(path))
+                {
+                    Console.Error.WriteLine("file not found.");
+                    return 1;
+                }
+
+                using (var inputStream = DatabaseReader.Read(new FileStream(path, FileMode.Open, FileAccess.Read), password))
+                using (var textReader = new StreamReader(inputStream, Encoding.UTF8))
+                {
+                    var document = new XmlDocument();
+                    document.LoadXml(textReader.ReadToEnd());
+                    var writer = new XmlTextWriter(Console.Out);
+                    writer.Formatting = Formatting.Indented;
+                    document.WriteContentTo(writer);
+                    return 0;
+                }
+            }
+            catch (IOException)
+            {
+                Console.Error.WriteLine("can not read file.");
+            }
+            catch (ArgumentException)
+            {
+                Console.Error.WriteLine("wrong password.");
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ SafeInCloudReader/SafeInCloudReader.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Ionic.Zlib;
+
+namespace SafeInCloudReader
+{
+    public class DatabaseReader
+    {
+        private const int KEY_LENGTH = 256;
+
+        private const int MAGIC = 1285;
+
+        private class Header
+        {
+            public short Magic;
+
+            public byte Version;
+
+            public byte[] Salt;
+
+            public byte[] Iv;
+
+            public byte[] SecretSalt;
+
+            public byte[] Secrets;
+        }
+
+        public static Stream Read(Stream inputStream, string password)
+        {
+            if (inputStream == null)
+            {
+                throw new ArgumentNullException(nameof(inputStream));
+            }
+
+            if (password == null)
+            {
+                throw new ArgumentNullException(nameof(password));
+            }
+
+            (byte[] secretKey, byte[] secretIv) = GetSecrets(inputStream, password);
+
+            using (var algorithm = GetAlgorithm(secretKey, secretIv))
+            {
+                Stream stream = new CryptoStream(inputStream, algorithm.CreateDecryptor(), CryptoStreamMode.Read);
+                stream = new BufferedStream(stream);
+                stream = new ZlibStream(stream, CompressionMode.Decompress);
+                return stream;
+            }
+        }
+
+        private static Header ReadHeader(BinaryReader input)
+        {
+            var header = new Header();
+
+            header.Magic = input.ReadInt16();
+            if (header.Magic != MAGIC)
+            {
+                throw new FormatException("unexpected magic value");
+            }
+
+            header.Version = input.ReadByte();
+            if (header.Version != 1)
+            {
+                throw new FormatException("unexpected sver value");
+            }
+
+            header.Salt = ReadByteArray(input);
+            header.Iv = ReadByteArray(input);
+            header.SecretSalt = ReadByteArray(input);
+            header.Secrets = ReadByteArray(input);
+
+            return header;
+        }
+
+        private static (byte[] Key, byte[] Iv) GetSecrets(Stream inputStream, string password)
+        {
+            using (var binaryReader = new BinaryReader(inputStream, Encoding.ASCII, leaveOpen: true))
+            {
+                Header header = ReadHeader(binaryReader);
+
+                byte[] passwordBytes = password.Select(x => (byte)x).ToArray();
+                byte[] key = GetKey(passwordBytes, header.Salt, 10000);
+
+                using (var algorithm = GetAlgorithm(key, header.Iv))
+                using (var decryptor = algorithm.CreateDecryptor())
+                {
+                    byte[] secrets = decryptor.TransformFinalBlock(header.Secrets, 0, header.Secrets.Length);
+                    using (var secretsReader = new BinaryReader(new MemoryStream(secrets)))
+                    {
+                        try
+                        {
+                            byte[] secretIv = ReadByteArray(secretsReader);
+                            byte[] secretKey = ReadByteArray(secretsReader);
+                            byte[] checkSum = ReadByteArray(secretsReader);
+                            byte[] calculatedCheckSum = GetKey(secretKey, header.SecretSalt, 1000);
+
+                            if (!checkSum.SequenceEqual(calculatedCheckSum))
+                            {
+                                throw new ArgumentException("wrong password");
+                            }
+
+                            return (secretKey, secretIv);
+                        }
+                        catch (EndOfStreamException)
+                        {
+                            throw new ArgumentException("wrong password");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static SymmetricAlgorithm GetAlgorithm(byte[] key, byte[] iv)
+        {
+            return new AesManaged
+            {
+                KeySize = KEY_LENGTH,
+                BlockSize = 128,
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.None,
+                Key = key,
+                IV = iv,
+            };
+        }
+
+        private static byte[] GetKey(byte[] password, byte[] salt, int iterationCount)
+        {
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, salt, iterationCount))
+            {
+                return deriveBytes.GetBytes(KEY_LENGTH / 8);
+            }
+        }
+
+        private static byte[] ReadByteArray(BinaryReader reader)
+        {
+            int length = reader.ReadByte();
+            return reader.ReadBytes(length);
+        }
+    }
+}
